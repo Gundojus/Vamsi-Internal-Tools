03e38ee26969e42d608ee1426527500b717a7f95
DMN-sohan <gundojusohan@gmail.com>
2025-06-07 16:14:52 +0530

    Updates

---
+++ precache-manifest.ddbc4aa40f57287988c59aab9b6c33ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af43b77fdf4e4c24871615fdc52c7d7b",
+    "url": "/Vamsi-Internal-Tools/index.html"
+  },
+  {
+    "revision": "c4509b14434e4196990c",
+    "url": "/Vamsi-Internal-Tools/static/css/2.05cbb0d5.chunk.css"
+  },
+  {
+    "revision": "36253db52b17929348d6",
+    "url": "/Vamsi-Internal-Tools/static/css/main.989288b7.chunk.css"
+  },
+  {
+    "revision": "c4509b14434e4196990c",
+    "url": "/Vamsi-Internal-Tools/static/js/2.3c92b724.chunk.js"
+  },
+  {
+    "revision": "1549e0aff47518b12dfe261410bcbef0",
+    "url": "/Vamsi-Internal-Tools/static/js/2.3c92b724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36253db52b17929348d6",
+    "url": "/Vamsi-Internal-Tools/static/js/main.f00cc2d0.chunk.js"
+  },
+  {
+    "revision": "f9e57532cb8f70bc1c3d",
+    "url": "/Vamsi-Internal-Tools/static/js/runtime-main.5d730a6c.js"
+  }
+]);
